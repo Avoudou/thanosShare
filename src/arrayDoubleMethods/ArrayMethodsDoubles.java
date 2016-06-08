ed54dfb3652322b1ff79658aa0541c433f596e88
Alexios <goddel@gmail.com>
2016-06-08 14:58:23 +0200

    waiting for instructions

--- src/arrayDoubleMethods/ArrayMethodsDoubles.java
+++ src/arrayDoubleMethods/ArrayMethodsDoubles.java
@@ -1,5 +1,33 @@
 package arrayDoubleMethods;
 
+import java.util.ArrayList;
+
 public class ArrayMethodsDoubles {
+
+	public ArrayList<Integer> getMaxValuesIndexes(Double[] arrayDoubles, int noOfMaxValues) {
+		ArrayList<Double> proxyList = new ArrayList<Double>();
+
+		for (int i = 0; i < arrayDoubles.length; i++) {
+			proxyList.add(arrayDoubles[i]);
+		}
+
+		return null;
+	}
+
+	private int getMaxValueIndex(ArrayList<Double> arrayListDoubles) {
+		double initial = arrayListDoubles.get(0);
+		int maxValueIndex = 0;
+		if (arrayListDoubles.size() == 1) {
+			return maxValueIndex;
+		}
+		for (int i = 1; i < arrayListDoubles.size(); i++) {
+			if (arrayListDoubles.get(maxValueIndex) - arrayListDoubles.get(i) > 0) {
+				maxValueIndex = i;
+			}
+
+		}
+
+		return maxValueIndex;
+	}
 
 }
---
+++ src/tests/Test.java
@@ -0,0 +1,11 @@
+package tests;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		double[] arrayDoubles = { 1, 2.3, 4.2 };
+
+	}
+
+}
