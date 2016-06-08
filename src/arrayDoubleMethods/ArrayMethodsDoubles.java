b95deabd5fbce19c89c150febaa34eb572ff1436
Alexios <goddel@gmail.com>
2016-06-08 15:59:14 +0200

    buggs

--- src/arrayDoubleMethods/ArrayMethodsDoubles.java
+++ src/arrayDoubleMethods/ArrayMethodsDoubles.java
@@ -6,16 +6,35 @@ public class ArrayMethodsDoubles {
 
 	public ArrayList<Integer> getMaxValuesIndexes(Double[] arrayDoubles, int noOfMaxValues) {
 		ArrayList<Double> proxyList = new ArrayList<Double>();
-
+		ArrayList<Double> unsortedMaxValues = new ArrayList<Double>();
 		for (int i = 0; i < arrayDoubles.length; i++) {
 			proxyList.add(arrayDoubles[i]);
 		}
 
-		return null;
+		ArrayList<Integer> maxValueIndexes = new ArrayList<Integer>();
+
+		for (int j = 0; j < noOfMaxValues; j++) {
+			int currentMaxValueIndex = getMaxValueIndex(proxyList);
+			maxValueIndexes.add(currentMaxValueIndex);
+			unsortedMaxValues.add(proxyList.get(currentMaxValueIndex));
+			proxyList.remove(currentMaxValueIndex);
+		}
+		for (int k = 0; k < unsortedMaxValues.size() - 1; k++) {
+			if (unsortedMaxValues.get(k) < unsortedMaxValues.get(k + 1)) {
+				double temp = unsortedMaxValues.get(k);
+				unsortedMaxValues.set(k, unsortedMaxValues.get(k + 1));
+				unsortedMaxValues.set(k + 1, temp);
+				int tempIndex = maxValueIndexes.get(k);
+				maxValueIndexes.set(k, maxValueIndexes.get(k + 1));
+				maxValueIndexes.set(k + 1, tempIndex);
+			}
+		}
+
+		return maxValueIndexes;
 	}
 
 	private int getMaxValueIndex(ArrayList<Double> arrayListDoubles) {
-		double initial = arrayListDoubles.get(0);
+
 		int maxValueIndex = 0;
 		if (arrayListDoubles.size() == 1) {
 			return maxValueIndex;
@@ -23,6 +42,7 @@ public class ArrayMethodsDoubles {
 		for (int i = 1; i < arrayListDoubles.size(); i++) {
 			if (arrayListDoubles.get(maxValueIndex) - arrayListDoubles.get(i) > 0) {
 				maxValueIndex = i;
+
 			}
 
 		}
--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,10 +1,17 @@
 package tests;
 
+import java.util.ArrayList;
+
+import arrayDoubleMethods.ArrayMethodsDoubles;
 
 public class Test {
 
 	public static void main(String[] args) {
-		double[] arrayDoubles = { 1, 2.3, 4.2 };
+		double[] arrayDoubles = { 1, 2.3, 4.2,8,5.2,7.3,6.5 };
+		
+		ArrayMethodsDoubles myMethods= new ArrayMethodsDoubles();
+		
+		ArrayList<Integer> sortedIndexes=
 
 	}
 
